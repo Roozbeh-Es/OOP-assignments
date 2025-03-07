1124d5e6530735b09c9a338b5bef05c3c3e3325f
Roozbeh-Es <rouzbehesmaeilpoor1@gmail.com>
2025-03-07 21:06:22 +0330

    completed the assignment

--- problem1.cpp
+++ problem1.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 
-void extractor(int n, int* arr) {
-    while(n>0) {
-        int hlp = n%10;
+void extractor(long long n, int* arr) {
+    while (n > 0) {
+        int hlp = n % 10;
         arr[hlp]++;
-        n/=10;
+        n /= 10;
     }
 }
 
 long long smallestNumber(int *arr) {
-    int small=0;
-    for(int i=0;i<10;i++) {
-        if(arr[i]>0) {
-            for(int j = 1; j<=arr[i]; j++) {
-                small*=10;
-                small+=i;
+    long long small = 0; /
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] > 0) {
+            for (int j = 1; j <= arr[i]; j++) {
+                small = small * 10 + i;
             }
         }
     }
     return small;
 }
+
 long long biggestNumber(int *arr) {
-    int big = 0;
-    for(int i=9;i>=0;i--) {
-        if(arr[i]>0) {
-            for(int j = 1; j<=arr[i]; j++) {
-                big = big*10+i;
+    long long big = 0;
+    for (int i = 9; i >= 0; i--) {
+        if (arr[i] > 0) {
+            for (int j = 1; j <= arr[i]; j++) {
+                big = big * 10 + i;
             }
         }
     }
@@ -34,11 +34,9 @@ long long biggestNumber(int *arr) {
 
 int main() {
     long long n;
-    std::cin>>n;
-    int arr[10];
-    for(int i=0;i<10;i++) {
-        arr[i]=0;
-    }
+    std::cin >> n;
+    int arr[10] = {0};
     extractor(n, arr);
-    std::cout<< smallestNumber(arr) + biggestNumber(arr);
+    std::cout << smallestNumber(arr) + biggestNumber(arr);
+    return 0;
 }
