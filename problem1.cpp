f630460a20c6e53b1ff384bb26dc3a9c688ff7bd
Roozbeh-Es <rouzbehesmaeilpoor1@gmail.com>
2025-03-07 20:56:49 +0330

    completed the assignment

--- problem1.cpp
+++ problem1.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 
+int digits(long long n) {
+    int count = 0;
+    while (n) {
+        n /= 10;
+        count++;
+    }
+    return count;
+}
+
 void extractor(int n, int* arr) {
     while(n>0) {
         int hlp = n%10;
-        arr[hlp-1]++;
+        arr[hlp]++;
         n/=10;
     }
 }
@@ -14,20 +23,31 @@ int smallestNumber(int *arr) {
         if(arr[i]>0) {
             for(int j = 1; j<=arr[i]; j++) {
                 small*=10;
-                small+=(i+1);
+                small+=i;
             }
         }
     }
     return small;
 }
+int biggestNumber(int *arr) {
+    int big = 0;
+    for(int i=9;i>=0;i--) {
+        if(arr[i]>0) {
+            for(int j = 1; j<=arr[i]; j++) {
+                big = big*10+i;
+            }
+        }
+    }
+    return big;
+}
 
 int main() {
-    int n;
+    long long n;
     std::cin>>n;
     int arr[10];
     for(int i=0;i<10;i++) {
         arr[i]=0;
     }
     extractor(n, arr);
-    std::cout<<smallestNumber(arr);
+    std::cout<< smallestNumber(arr) + biggestNumber(arr);
 }
