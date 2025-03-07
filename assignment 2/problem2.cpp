4dea4b7f86afb8497506ffa1acd2b053f16cebb5
Roozbeh-Es <rouzbehesmaeilpoor1@gmail.com>
2025-03-07 22:25:29 +0330

    changes

--- assignment 2/problem2.cpp
+++ assignment 2/problem2.cpp
@@ -1,62 +1,64 @@
 #include <bits/stdc++.h>
 
-std::vector<std::pair<int,std::string>> vec;
 
 int main() {
+    std::vector<std::pair<int, std::string> > vec;
+
     std::string s;
-    while(getline(std::cin, s)) {
-        if(s!="end") {
+    while (getline(std::cin, s)) {
+        if (s != "end") {
             std::stringstream ss(s);
             int hlp1;
             std::string hlp2;
             ss >> hlp1 >> hlp2;
-            vec.emplace_back(hlp1,hlp2);
-        }
-        else {
+            vec.emplace_back(hlp1, hlp2);
+        } else {
             break;
         }
     }
 
 
-    int k,t;
-    std::cin>>k>>t;
+    int k, t;
+    std::cin >> k >> t;
 
     int start = 0;
     int sum = 0;
     bool ans = false;
 
-    for(int i=0; i<vec.size(); i++) {
-        if(ans)
+    for (int i = 0; i < vec.size(); i++) {
+        if (ans)
             break;
-        if(vec[i].second == "Gold") {
-            start = i+1;
+        if (vec[i].second == "Gold") {
+            bool prob = false;
+            start = i + 1;
             sum = vec[i].first;
-            for(int j=1; j<=k; j++) {
-                if(vec[i+j].second == "Gold") {
-                    sum += vec[i+j].first;
-                }
-                else
-                    break;
-                if(sum == t) {
-                    ans = true;
-                    std::cout<<start<<std::endl;
+            for (int j = 1; j <= k; j++) {
+                if (vec[i + j].second == "Gold") {
+                    sum += vec[i + j].first;
+                } else {
+                    prob = true;
                     break;
                 }
+            }
+            if (sum == t && !prob) {
+                ans = true;
+                std::cout << start << std::endl;
+                break;
             }
         }
     }
-    if(!ans) {
-        int min = 0;
-        for(int i=0; i<vec.size(); i++) {
+    if (!ans) {
+        int min = INT_MAX;
+        for (int i = 0; i < vec.size(); i++) {
             int hlp = vec[i].first;
-            for(int j=1; j<=k; j++) {
-                hlp += vec[i+j].first;
+            for (int j = 1; j <= k; j++) {
+                hlp += vec[i + j].first;
             }
-            if(hlp<min) {
+            if (hlp < min) {
                 min = hlp;
-                start = i+1;
+                start = i + 1;
             }
         }
-        std::cout<<start<<std::endl;
+        std::cout << start << std::endl;
     }
 }
