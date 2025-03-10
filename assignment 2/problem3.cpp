fac2974aefc42e4d0069f8dfcf90a2cb975a0aea
Roozbeh-Es <rouzbehesmaeilpoor1@gmail.com>
2025-03-10 10:43:38 +0330

    possible fixes

--- assignment 2/problem3.cpp
+++ assignment 2/problem3.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
-long long* insert(const long long *arr, int &currentSize,long long value) {
-    long long* newArr = new long long[currentSize+1];
+long long *insert(const long long *arr, int &currentSize, long long value) {
+    long long *newArr = new long long[currentSize + 1];
     for (int i = 0; i < currentSize; i++) {
         newArr[i] = arr[i];
     }
@@ -11,11 +11,11 @@ long long* insert(const long long *arr, int &currentSize,long long value) {
     return newArr;
 }
 
-long long* pop(long long* arr, int pos, int& currentSize) {
-    if(pos>currentSize || (pos == 0 && currentSize == 0)) {
+long long *pop(long long *arr, int pos, int &currentSize) {
+    if (pos > currentSize || (pos == 0 && currentSize == 0)) {
         return arr;
     }
-    long long* newArr = new long long[currentSize - 1];
+    long long *newArr = new long long[currentSize - 1];
     for (int i = 0, j = 0; i < currentSize; i++) {
         if (i != pos) {
             newArr[j++] = arr[i];
@@ -29,33 +29,33 @@ long long* pop(long long* arr, int pos, int& currentSize) {
 
 
 int main() {
-    long long* arr = nullptr;
+    long long *arr = nullptr;
     int currentSize = 0;
 
     std::string s;
-    while(std::getline(std::cin, s)) {
-        if(s=="exit")
+    while (std::getline(std::cin, s)) {
+        if (s == "exit")
             break;
-        if(s.find("insert") == 0) {
+        if (s.find("insert") == 0) {
             long long value = std::stoll(s.substr(7, s.length()));
             arr = insert(arr, currentSize, value);
-        }
-        else if(s.find("pop") == 0) {
+        } else if (s.find("pop") == 0) {
             int pos = std::stoi(s.substr(4, s.length()));
             arr = pop(arr, pos, currentSize);
-        }
-        else if(s.find("print") == 0) {
+        } else if (s.find("print") == 0) {
             for (int i = 0; i < currentSize; i++) {
                 std::cout << arr[i] << " ";
             }
-            std::cout<<std::endl;
-        }
-        else if(s.find("swap") == 0) {
+            std::cout << std::endl;
+        } else if (s.find("swap") == 0) {
             std::string trash;
             int pos1, pos2;
             std::stringstream ss(s);
-            ss>>trash>>pos1>>pos2;
-            std::swap(arr[pos1], arr[pos2]);
+            ss >> trash >> pos1 >> pos2;
+            if (pos1 > currentSize || pos2 > currentSize || (pos1 == 0 && pos2 == 0 && currentSize == 0))
+                continue;
+            else
+                std::swap(arr[pos1], arr[pos2]);
         }
     }
 }
